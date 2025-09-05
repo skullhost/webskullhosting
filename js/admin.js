a926a2799df91d1745e4eab5fafc3aa967cf067c
skullhost <skulltiktok21@gmail.com>
2025-09-05 15:53:37 +0700

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://YOUR_PROJECT_ID.supabase.co";
+const SUPABASE_URL = "https://zvqlsgwccrdqjgcxgmzq.supabase.co";
 const SUPABASE_KEY = "YOUR_SERVICE_ROLE_OR_ANON_KEY";
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
